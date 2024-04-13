b3967af38e378d7cc0292de51da7b0c1796174d2
markkhomenko <silverstone909@tutamail.com>
2024-04-14 00:48:04 +0300

    update api documentation

---
+++ src/plogin.php
@@ -0,0 +1,24 @@
+<?php
+
+$cas_host = "passport.ustc.edu.cn";
+$cas_port = 443;
+$cas_context = "";
+
+require_once './cas-client/CAS.php';
+
+phpCAS::client(CAS_VERSION_2_0, $cas_host, $cas_port, $cas_context);
+//$url="http://".$_SERVER["HTTP_HOST"].$_SERVER['PHP_SELF'];
+$url = "https://adrain.ustclug.org/plogin.php";
+phpCAS::setFixedServiceURL($url);
+phpCAS::setNoCasServerValidation();
+phpCAS::forceAuthentication();
+$user = phpCAS::getUser();
+$attributes = phpCAS::getAttributes();
+
+$studentnumber = addslashes($user);
+$userid = $studentnumber;
+
+$expire = time() + 900;
+require './anonymous_config.php';
+
+header('location: https://adrain.ustclug.org/login.php?userid=' . sha1($key . '|' . $expire) . '&expire=' . $expire);
