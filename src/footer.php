86a768f67a0b14f59ffc1f975788fc498d9223eb
markkhomenko <silverstone909@tutamail.com>
2024-04-20 00:48:04 +0300

    Improve error handling in services

---
+++ src/footer.php
@@ -0,0 +1,24 @@
+<?php
+if (!defined('IN_ADRAIN')) {
+    exit('Access Denied');
+}
+
+?>
+
+    </div> <!-- /container -->
+
+	<br/>
+	<div class="container" style="color: #444;text-align:center;">
+	<p>2017 - 2022 Admission Rain LUG@USTC <a href="update.php">(TOKEN)</a></p>
+	<p>The University of Science and Technology of China is a first-class university. We need to support our school.</p>
+    <p>Website admin contact: lug AT ustc.edu.cn</p>
+	</div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="/static/jquery/1.11.2/jquery.min.js"></script>
+    <script src="/static/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+
+  </body>
+</html>
